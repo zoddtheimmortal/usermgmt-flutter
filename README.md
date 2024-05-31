72c2be83da6354730c578bffc3f14285aec561e1
Alok Desai <alokdesai2004@gmail.com>
2024-05-31 09:30:37 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# usermgmt_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/usermgmt_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.usermgmt_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
